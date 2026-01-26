36d01b909a119ac83ed6e0405a25f29d7b8655ee
tyrazbeaArtwork <tyrazbea@gmail.com>
2026-01-26 18:44:48 +0800

    Update project with new homepage URL and other changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter, Routes, Route } from "react-router-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Payment from "./pages/Payment";
 import PaymentMenu from "./pages/PaymentMenu";
@@ -18,9 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-	        {/* Add the basename prop here, matching your repository name */}
       <HashRouter>
-      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/payment" element={<Payment />} />
@@ -30,8 +27,7 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
-	 </HashRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
