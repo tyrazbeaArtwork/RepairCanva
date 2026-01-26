6da3f8cdb04d82f385f404e530f61f536c5c2bf9
tyrazbeaArtwork <tyrazbea@gmail.com>
2026-01-26 18:03:28 +0800

    Update project with new homepage URL and other changes

--- src/App.tsx
+++ src/App.tsx
@@ -30,8 +30,8 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-		      </HashRouter>
       </BrowserRouter>
+	 </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
